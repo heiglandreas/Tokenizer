37db32446ee8dacd2d0835d4677271c4fee1726a
Andreas Heigl <andreas@heigl.org>
2014-01-07 16:19:42 +0100

    Adds a CamelCase-Tokenizer

---
+++ src/Org_Heigl/Tokenizer/Tokenizers/CamelCaseTokenizer.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright (c)2013-2013 heiglandreas
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIBILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category 
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright ©2013-2013 Andreas Heigl
+ * @license   http://www.opesource.org/licenses/mit-license.php MIT-License
+ * @version   0.0
+ * @since     15.12.13
+ * @link      https://github.com/heiglandreas/Tokenizer
+ */
+
+namespace Org_Heigl\Tokenizer\Tokenizers;
+
+use Org_Heigl\Tokenizer\TokenizerInterface;
+use Org_Heigl\Tokenizer\TokenList;
+use Org_Heigl\Tokenizer\Token;
+
+/**
+ * This class splits incoming strings on every whitespace-occurence.
+ *
+ * A whitespace is by default any of the following:
+ *
+ * * space
+ * * no-breaking space
+ * * carriage-return
+ * * line-feed
+ * * any kind of space in differing widths
+ *
+ * @package Org_Heigl\Tokenizer\Tokenizers
+ */
+class CamelCaseTokenizer extends AbstractTokenizer
+{
+    /**
+     * Tokenize a given token
+     *
+     * @param Token $token
+     *
+     * @return TokenList
+     */
+    public function tokenizeToken(Token $token)
+    {
+        $tokenList = new TokenList();
+        $string = (string) $token;
+
+        if (! preg_match_all('/((?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z]))/u', $string, $matches, PREG_SET_ORDER ^ PREG_OFFSET_CAPTURE)) {
+//        if (! preg_match_all('/(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])/u', $string, $matches, PREG_SET_ORDER ^ PREG_OFFSET_CAPTURE)) {
+//        if (! preg_match_all('/(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])/u', $string, $matches, PREG_SET_ORDER ^ PREG_OFFSET_CAPTURE)) {
+//        if (! preg_match_all('/([A-Z]+[^A-Z]*)/u', $string, $matches, PREG_SET_ORDER ^ PREG_OFFSET_CAPTURE)) {
+            return $tokenList->add($token);
+        }
+
+        $offsets = array();
+        foreach ($matches as $tok) {
+            $offsets[] = $tok[1][1];
+        }
+        $offsets[] = strlen($string);
+
+        $start = 0;
+        foreach ($offsets as $offset) {
+
+            $tokenString = substr($string, $start, $offset - $start);
+            $type = 'string';
+            $tokenList->add(Token::create($start + $token->getOffset(), $tokenString, $type));
+            $start = $offset;
+        }
+        
+        return $tokenList;
+    }
+
+} 
---
+++ tests/Org_Heigl/TokenizerTest/Tokenizers/CamelCaseTokenizerTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Copyright (c)2013-2013 heiglandreas
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIBILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category 
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright ©2013-2013 Andreas Heigl
+ * @license   http://www.opesource.org/licenses/mit-license.php MIT-License
+ * @version   0.0
+ * @since     15.12.13
+ * @link      https://github.com/heiglandreas/
+ */
+
+namespace Org_Heigl\TokenizerTest\Tokenizers;
+
+
+use Org_Heigl\Tokenizer\Tokenizers\CamelCaseTokenizer;
+use Org_Heigl\Tokenizer\Token;
+use Org_Heigl\Tokenizer\TokenList;
+
+class CamelCaseTokenizerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCamelCaseTokenizer()
+    {
+        $tokenizer= new CamelCaseTokenizer();
+
+        $token = Token::create(0, 'dasIstEinEinfacherTest', 'string');
+        $tokenList = $tokenizer->tokenizeToken($token);
+
+        $this->assertInstanceOf('Org_Heigl\Tokenizer\TokenList', $tokenList);
+        $this->assertEquals(5, $tokenList->count());
+        $this->assertEquals('Ist', $tokenList->offsetGet(1)->getToken());
+        $this->assertEquals('Einfacher', $tokenList->offsetGet(3)->getToken());
+        $this->assertEquals('dasIstEinEinfacherTest', (string) $tokenList);
+    }
+
+    public function testTokenizerWithDoubleUppercaseWhitespace()
+    {
+        $tokenizer= new CamelCaseTokenizer();
+
+        $token = Token::create(0, 'DasIstEinLDAPTest', 'string');
+        $tokenList = $tokenizer->tokenizeToken($token);
+        $this->assertInstanceOf('Org_Heigl\Tokenizer\TokenList', $tokenList);
+        $this->assertEquals(5, $tokenList->count());
+        $this->assertEquals('Ist', $tokenList->offsetGet(1)->getToken());
+        $this->assertEquals('LDAP', $tokenList->offsetGet(3)->getToken());
+        $this->assertEquals('Test', $tokenList->offsetGet(4)->getToken());
+        $this->assertEquals('DasIstEinLDAPTest', (string) $tokenList);
+    }
+
+    public function testTokenizerReturnsUntokenizableToken()
+    {
+        $tokenizer= new CamelCaseTokenizer();
+
+        $token = Token::create(0, 'seefähre', 'string');
+        $tokenList = $tokenizer->tokenizeToken($token);
+
+        $this->assertInstanceOf('Org_Heigl\Tokenizer\TokenList', $tokenList);
+        $this->assertEquals(1, $tokenList->count());
+
+    }
+
+    public function testWhitespaceTokenizerSimpleCall()
+    {
+        $tokenizer= new CamelCaseTokenizer();
+
+        $token = Token::create(0, 'DasIstEinEinfacherTest', 'string');
+        $tokenL = new TokenList();
+        $tokenL->add($token);
+        $tokenList = $tokenizer->tokenize($tokenL);
+
+        $this->assertInstanceOf('Org_Heigl\Tokenizer\TokenList', $tokenList);
+        $this->assertSame($tokenL, $tokenList);
+        $this->assertEquals(5, $tokenList->count());
+        $this->assertEquals('Ist', $tokenList->offsetGet(1)->getToken());
+        $this->assertEquals('Einfacher', $tokenList->offsetGet(3)->getToken());
+        $this->assertEquals('DasIstEinEinfacherTest', (string) $tokenList);
+    }
+
+
+
+}
+ 
